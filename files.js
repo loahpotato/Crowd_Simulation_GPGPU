e4cfbb54dab683ea72ee0ddb93ac34de5e7d8a4e
Siyi Liu <964040805@qq.com>
2023-02-03 22:22:24 +0100

    clean up

--- files.js
+++
@@ -1,9 +0,0 @@
-var files = {
-	"webgl": [
-	    "webgl_gpgpu_erosion",
-		"webgl_gpgpu_water",
-        "webgl_gpgpu_protoplanet",
-        "webgl_gpgpu_birds",
-        "webgl_physics_convex_break"
-	]
-};
